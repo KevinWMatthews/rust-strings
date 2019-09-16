5105e302bb51e6b2a00daf0ed31e613d8c5a665a
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-15 22:08:50 -0700

    Add type checking example to format.rs

--- src/bin/format.rs
+++ src/bin/format.rs
@@ -3,6 +3,8 @@ fn main() {
     let two = String::from("two");
 
     // https://doc.rust-lang.org/std/macro.format.html
-    let result = format!("{} {}", one, two);
+    let string = format!("{} {}", one, two);
+    // let type_check: () = result;
+    let result: String = string;
     println!("result: '{}'", result);
 }
